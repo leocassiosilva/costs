4ec16b2f2df0f461994fcc0710c5fe85452d0227
leocassiosilva <leocassiosilva1234@gmail.com>
2022-05-03 21:10:15 -0300

    message

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,11 +1,34 @@
 import styles from './NewProject.module.css';
 import ProjectForm from '../project/Projectform';
+import { useHistory } from 'react-router-dom';
 function NewProject() {
+    const history = useHistory();
+
+    function createPost(project) {
+        //inicialize cost and services
+        project.cost = 0;
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                //redirect
+                history.push('/projects', { message: 'Projeto Criado com Sucesso!' })
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto" />
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
     );
 }
